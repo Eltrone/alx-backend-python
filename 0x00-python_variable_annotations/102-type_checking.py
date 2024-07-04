7707d5a2f9392699e3256d28aced70d31b9b59b7
Eltrone <amineelorche@gmail.com>
2024-07-04 22:03:51 +0000

    file added

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,25 +1,17 @@
 #!/usr/bin/env python3
 """Type Checking"""
-from typing import Tuple, List, Union
+from typing import Tuple, List
 
 
-def zoom_array(lst: Union[Tuple[int, ...], List[int]],
-               factor: int = 2) -> List[int]:
-    """
-    Increase the size of a sequence by repeating its elements.
-
-    Args:
-        lst (Union[Tuple[int, ...], List[int]]): Sequence to zoom.
-        factor (int): How many times to repeat each element.
-
-    Returns:
-        List[int]: List where each element is repeated 'factor' times.
-    """
-    zoomed_in: List[int] = [item for item in lst for _ in range(factor)]
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """zoom_array function"""
+    zoomed_in: List = [item for item in lst for i in
+                       range(factor)]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
+
 zoom_3x = zoom_array(array, 3)
