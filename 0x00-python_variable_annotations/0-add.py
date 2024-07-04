c45164f648ad3c2adcf7e0786ee0ab9d9d234a98
Eltrone <amineelorche@gmail.com>
2024-07-04 16:22:38 +0000

    file added

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Module to perform addition of two float numbers.
+"""
+
+def add(a: float, b: float) -> float:
+    """Add two float numbers and return the result.
+
+    Args:
+    a (float): The first number.
+    b (float): The second number.
+
+    Returns:
+    float: The sum of the two numbers.
+    """
+    return a + b
+
+if __name__ == "__main__":
+    add = __import__('0-add').add
+    print(add(1.11, 2.22) == 1.11 + 2.22)
+    print(add.__annotations__)
