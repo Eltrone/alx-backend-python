f69e3f87041496fb969288c90c1a89b00f72e583
Eltrone <amineelorche@gmail.com>
2024-07-04 22:02:39 +0000

    file added

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -2,9 +2,10 @@
 """Type Checking"""
 from typing import Tuple, List, Union
 
-def zoom_array(lst: Union[Tuple[int, ...], List[int]], factor: int = 2) -> List[int]:
-    """zoom_array function
 
+def zoom_array(lst: Union[Tuple[int, ...], List[int]],
+               factor: int = 2) -> List[int]:
+    """
     Increase the size of a sequence by repeating its elements.
 
     Args:
@@ -17,8 +18,8 @@ def zoom_array(lst: Union[Tuple[int, ...], List[int]], factor: int = 2) -> List[
     zoomed_in: List[int] = [item for item in lst for _ in range(factor)]
     return zoomed_in
 
+
 array = [12, 72, 91]
 
 zoom_2x = zoom_array(array)
-
 zoom_3x = zoom_array(array, 3)
