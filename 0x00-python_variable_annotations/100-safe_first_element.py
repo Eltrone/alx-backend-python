346ba0d1450a03b81a2d232f3c724fc59f643f3a
Eltrone <amineelorche@gmail.com>
2024-07-04 21:56:14 +0000

    file added

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
-# Module to safely get the first element of a sequence.
-from typing import Any, Sequence, Union, Optional
-
+"""Duck typing - first element of a sequence"""
+from typing import Sequence, Any, Union, Optional
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
-    """Return the first element of a sequence, or None if the sequence is empty.
+    """Duck typing - first element of a sequence
 
     Args:
-    lst (Sequence[Any]): The sequence.
+        lst (Sequence[Any]): The sequence from which the first element is returned.
 
     Returns:
-    Optional[Any]: The first element of the sequence or None.
+        Optional[Any]: The first element of the sequence or None if the sequence is empty.
     """
     if lst:
         return lst[0]
