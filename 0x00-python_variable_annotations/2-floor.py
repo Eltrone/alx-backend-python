ba3c45bcd42c8510242e3cad2aad5c6b161ca151
Eltrone <amineelorche@gmail.com>
2024-07-04 16:27:42 +0000

    file added

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module to find the floor of a float number.
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of the float n.
+
+    Args:
+    n (float): The number to floor.
+
+    Returns:
+    int: The floor of the number.
+    """
+    return math.floor(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module to convert a float to its string representation.
+"""
+
+
+def to_str(n: float) -> str:
+    """Convert a float to string and return it.
+
+    Args:
+    n (float): The float to convert.
+
+    Returns:
+    str: The string representation of the float.
+    """
+    return str(n)
