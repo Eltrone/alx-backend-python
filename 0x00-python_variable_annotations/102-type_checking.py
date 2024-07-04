b01f60935167ad412b24f7e9be6fe817b08b80db
Eltrone <amineelorche@gmail.com>
2024-07-04 22:08:38 +0000

    file added

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,28 +1,13 @@
-i#!/usr/bin/env python3
-"""Type Checking"""
-# Import Tuple and List types for type annotations.
+#!/usr/bin/env python3
+"""Defines a function that expands an array by repeating its elements."""
+
 from typing import Tuple, List
 
+
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """
-    Zooms into an array by repeating each element.
+    return [item for item in lst for _ in range(factor)]
 
-    Args:
-        lst (Tuple): Tuple containing elements to repeat.
-        factor (int, optional)
-
-    Returns:
-        List: Contains repeated elements.
-    """
-    # Create a list with each element of lst repeated 'factor' times.
-    zoomed_in: List = [item for item in lst for i in range(factor)]
-    return zoomed_in
-
-# Define a tuple 'array' with three integers.
+# Example usage
 array = (12, 72, 91)
-
-# Call function with the array and default factor.
-zoom_2x = zoom_array(array)
-
-# Call function with the array and a factor of 3.
-zoom_3x = zoom_array(array, 3)
+zoom_2x = zoom_array(array)  # Default factor of 2
+zoom_3x = zoom_array(array, 3)  # Custom factor of 3
