fd6ff20da9e6135f9b22add524a4f249e7b375de
Eltrone <amineelorche@gmail.com>
2024-07-29 15:26:32 +0000

    file modified

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -66,8 +66,8 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         cls.mock = cls.get_patcher.start()
         cls.mock.return_value = Mock()
         cls.mock.return_value.json.side_effect = [
-            lambda: cls.org_payload,
-            lambda: cls.repos_payload
+            cls.org_payload,
+            cls.repos_payload
         ]
 
     @classmethod
@@ -85,6 +85,26 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
         client = GithubOrgClient("org")
         self.assertEqual(client.public_repos(license="apache-2.0"),
                          self.apache2_repos)
+
+
+class GithubOrgClient:
+    ORG_URL = "https://api.github.com/orgs/{org}"
+
+    def __init__(self, org_name):
+        self._org_name = org_name
+
+    @property
+    @memoize
+    def org(self):
+        """ Return processed org information """
+        url = self.ORG_URL.format(org=self._org_name)
+        return get_json(url)
+
+    @property
+    @memoize
+    def _public_repos_url(self):
+        """ Return the public repos URL. """
+        return self.org["repos_url"]
 
 
 if __name__ == '__main__':
